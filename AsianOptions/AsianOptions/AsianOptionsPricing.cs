8d441aebeb170f91e9d21c4b49d41abab55f546b
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-31 03:30:59 +0900

    Implemented some code to do simulation.

---
+++ AsianOptions/AsianOptions/AsianOptionsPricing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AsianOptions
+{
+    /// <summary>
+    /// Calculates the price of an asian options
+    /// based on the given statistic context.
+    /// </summary>
+    internal class AsianOptionsPricing
+    {
+        /// <summary>
+        /// Calculates the price of an option with the given statistical info.
+        /// The algorithm used for calculation is the Monte Carlo method.
+        /// </summary>
+        /// <param name="rand">Random number generator</param>
+        /// <param name="initial">Initial price</param>
+        /// <param name="exercise">Excercise price</param>
+        /// <param name="up">Up growth</param>
+        /// <param name="down">Down growth</param>
+        /// <param name="interest">Interest rate</param>
+        /// <param name="periods">Number of periods to maturity</param>
+        /// <param name="sims">Number of simulations to perform</param>
+        /// <returns>The calculated value for an option with the given
+        /// statistical context using the Monte Carlo method.</returns>
+        internal static double Simulation(
+            Random rand, double initial, double exercise,
+            double up, double down, double interest, long periods, long sims)
+        {
+            //TODO Risk-neutral probabilities:
+
+            double sum = 0.0;
+
+            //TODO Run simulations:
+
+            // Return average across all simulations:
+            return (sum / Math.Pow(interest, periods)) / sims;
+        }
+    }
+}
--- AsianOptions/AsianOptions/MainWindow.xaml.cs
+++ AsianOptions/AsianOptions/MainWindow.xaml.cs
@@ -17,24 +17,61 @@ namespace AsianOptions
             cmdPriceOption.Click += cmdPriceOption_Click;
         }
 
-        //TODO
-
-        private void cmdPriceOption_Click(object sender, RoutedEventArgs e)
-        {
-            //TODO
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Exit the app.
+        /// </summary>
         private void mnuFileExit_Click(object sender, RoutedEventArgs e)
         {
-            //TODO
-            throw new NotImplementedException();
+            // trigger "closed" event as if user had hit "X" on window:
+            Close();
         }
 
         private void mnuFileSave_Click(object sender, RoutedEventArgs e)
         {
             //TODO
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Main button to run the simulation.
+        /// </summary>
+        private void cmdPriceOption_Click(object sender, RoutedEventArgs e)
+        {
+            cmdPriceOption.IsEnabled = false;
+
+            //TODO spinner settings
+
+            double initial = Convert.ToDouble(txtInitialPrice.Text);
+            double exercise = Convert.ToDouble(txtExercisePrice.Text);
+            double up = Convert.ToDouble(txtUpGrowth.Text);
+            double down = Convert.ToDouble(txtDownGrowth.Text);
+            double interest = Convert.ToDouble(txtInterestRate.Text);
+            long periods = Convert.ToInt64(txtPeriods.Text);
+            long sims = Convert.ToInt64(txtSimulations.Text);
+
+            //
+            // Run simulation to price options:
+            //
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            int start = Environment.TickCount;
+
+            double price = AsianOptionsPricing.Simulation(
+                rand, initial, exercise, up, down, interest, periods, sims);
+
+            int stop = Environment.TickCount;
+
+            double elapsedTimeInSecs = (stop - start) / 1000.0;
+
+            var result = $"{price:C} [{elapsedTimeInSecs:#,##0.00} secs]";
+
+            //
+            // Display the results:
+            //
+            lstPrices.Items.Insert(0, result);
+
+            //TODO spinner settings
+
+            cmdPriceOption.IsEnabled = true;
         }
     }
 }
