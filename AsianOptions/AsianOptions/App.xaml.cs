a3ee8e6c213d0650cb8d1bf9399c6d168f16dd0a
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-31 06:43:08 +0900

    Initialized the CultureInfo for this app.

--- AsianOptions/AsianOptions/App.xaml.cs
+++ AsianOptions/AsianOptions/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
 
 namespace AsianOptions
 {
@@ -7,6 +8,14 @@ namespace AsianOptions
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // How to change Application Culture in wpf?
+            // https://stackoverflow.com/questions/24135675/how-to-change-application-culture-in-wpf
+            var culture = new CultureInfo("en");
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
